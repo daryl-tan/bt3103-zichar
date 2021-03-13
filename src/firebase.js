38df8212b7090d98859effd506358b5c914d937a
Daryl Tan <daryltan900@gmail.com>
2021-03-13 18:18:45 +0800

    updated firebase info again

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
-var firebaseConfig = {
-	apiKey: "AIzaSyCV3iBPerBC-sjhWiuFoVdD7SPxZ7pc1nY",
-	authDomain: "bt3103-zichar.firebaseapp.com",
-	projectId: "bt3103-zichar",
-	storageBucket: "bt3103-zichar.appspot.com",
-	messagingSenderId: "324107074590",
-	appId: "1:324107074590:web:446e5b5f0fff5b0c23020f",
+const firebaseConfig = {
+	apiKey: "AIzaSyD2hS9nyMncHvs2Ak1TrYyCz6CJb0XsIFM",
+	authDomain: "bt3103-zichar-ab201.firebaseapp.com",
+	projectId: "bt3103-zichar-ab201",
+	storageBucket: "bt3103-zichar-ab201.appspot.com",
+	messagingSenderId: "22239220444",
+	appId: "1:22239220444:web:8529764992534644255a19",
 };
 firebase.initializeApp(firebaseConfig);
 var database = firebase.firestore();
